c51b728bfd1c1b9d4827b5fc52a2e84d5d38c3cf
Abdu <abdukr5@gmail.com>
2021-02-01 11:54:01 -0600

    ParentProfileViewModel added

---
+++ NashvilleBabysitter/Models/ViewModels/ParentProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NashvilleBabysitter.Models.ViewModels
+{
+    public class ParentProfileViewModel
+    {
+        public UserProfile UserProfile { get; set; }
+        public List<Child> Children { get; set; }
+        public List<UserProfile> Babysitters { get; set; }
+        public List<Babysit> Babysits { get; set; }
+
+    }
+}
