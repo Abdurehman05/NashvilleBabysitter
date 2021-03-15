85e1f0629d064c5388498fab963583c049de834a
Abdu <abdukr5@gmail.com>
2021-03-14 22:52:12 -0500

    add detail link on on child name

--- NashvilleBabysitter/client/src/components/ChildCard.js
+++ NashvilleBabysitter/client/src/components/ChildCard.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card, Image, Header } from "semantic-ui-react";
 
 const ChildCard = ({ child }) => (
   <Card>
     <Image src={child.imageUrl} />
-    <Header>{child.name}</Header>
+    <Link to={`child/detail/${child.id}`}>
+      <Header>{child.name}</Header>
+    </Link>
     <Card.Content>Age: {child.age}</Card.Content>
     <Card.Content>
       <strong>Note:</strong> {child.notes}
