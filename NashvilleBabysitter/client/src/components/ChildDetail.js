98b6da14d4ce8f15c1d1f5d5aa2e1ea44832841a
Abdu <abdukr5@gmail.com>
2021-04-15 19:01:35 -0500

    child detail

--- NashvilleBabysitter/client/src/components/ChildDetail.js
+++ NashvilleBabysitter/client/src/components/ChildDetail.js
@@ -5,5 +5,22 @@ const ChildDetail = () => {
   const [child, setChild] = useState();
   const history = useHistory();
   const { childId } = useParams();
+
+  const currentUser = JSON.parse(localStorage.getItem("userProfile")).id;
+
+  useEffect(() => {
+    getToken().then((token) =>
+      fetch(`/babysitter/${currentUser}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((child) => {
+          setChild(child);
+        })
+    );
+  }, []);
 };
 export default ChildDetail;
