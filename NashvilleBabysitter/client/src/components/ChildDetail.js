cec0b939b6ad4d5f91c968a7bfb6b65ba5c85694
Abdu <abdukr5@gmail.com>
2021-04-28 00:39:32 -0500

    Application view and ChildDetail files updated

--- NashvilleBabysitter/client/src/components/ChildDetail.js
+++ NashvilleBabysitter/client/src/components/ChildDetail.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
 
 const ChildDetail = () => {
   const [child, setChild] = useState();
   const history = useHistory();
   const { childId } = useParams();
+  const { getToken } = useContext(UserProfileContext);
 
   const currentUser = JSON.parse(localStorage.getItem("userProfile")).id;
 
